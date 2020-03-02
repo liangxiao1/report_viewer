74b0a0a81b84f5f92f4c7d722a8e4e650c08d919
Charles Shih <schrht@gmail.com>
2020-03-02 16:05:07 +0800

    Using UpperCamelCase and pep8 formatting (#3)
    * Add .vscode/settings.json
    
    * update README.rst
    
    * Add utils/ali_report_write.py
    
    * Pep-8 Format
    
    * Add .gitignore
    
    * Add AliReport.
    
    * Remove % for the filter
    
    * Rename README.rst to README.md.
    
    * Update README.md
    
    * Update README.md
    
    * Add .vscode/settings.json
    
    * Add utils/ali_report_write.py
    
    * Pep-8 Format
    
    * Add .gitignore
    
    * Add AliReport.
    
    * Remove % for the filter
    
    * Rename README.rst to README.md.
    
    * Update README.md
    
    * yapf for pep8 format
    
    * Add Alibaba Support.
    
    * Rename class AliReport to Ali_Report.
    
    * a small bugfix
    
    * Add platform field.
    
    * Change the description.
    
    * Specify the tablename
    
    * Add Alibaba support. (#2)
    
    * Add .vscode/settings.json
    
    * update README.rst
    
    * Add utils/ali_report_write.py
    
    * Pep-8 Format
    
    * Add .gitignore
    
    * Add AliReport.
    
    * Remove % for the filter
    
    * Rename README.rst to README.md.
    
    * Update README.md
    
    * Update README.md
    
    * Add .vscode/settings.json
    
    * Add utils/ali_report_write.py
    
    * Pep-8 Format
    
    * Add .gitignore
    
    * Add AliReport.
    
    * Remove % for the filter
    
    * Rename README.rst to README.md.
    
    * Update README.md
    
    * yapf for pep8 format
    
    * Add Alibaba Support.
    
    * Rename class AliReport to Ali_Report.
    
    * a small bugfix
    
    * update ali_report_write.py
    
    * Using UpperCamelCase.
    
    * pep8 formatting.

--- app/__init__.py
+++ app/__init__.py
@@ -10,11 +10,10 @@ from app.commen import MyIndexView
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 
-
 app = Flask(__name__)
 app.config.from_object("config")
 db = SQLA(app)
-#appbuilder = AppBuilder(app, db.session)
+# appbuilder = AppBuilder(app, db.session)
 appbuilder = AppBuilder(app, db.session, indexview=MyIndexView)
 
 
--- app/models.py
+++ app/models.py
@@ -3,8 +3,10 @@ from flask_appbuilder import Model
 from flask_appbuilder.filemanager import ImageManager
 from flask_appbuilder.models.mixins import ImageColumn, BaseMixin
 from flask_appbuilder.security.sqla.models import User
-from sqlalchemy import Column, Float, ForeignKey, Integer, String, Text, Date, MetaData
+from sqlalchemy import (Column, ForeignKey, Integer, String, Text, Date, Float,
+                        MetaData)
 from sqlalchemy.orm import relationship
+
 
 class EC2_Report(Model):
     '''
@@ -33,11 +35,39 @@ class EC2_Report(Model):
         return self.log_id
 
     def result_url(self):
-        return Markup(
-            '<a href="' +
-            self.report_url +
-            '">result</a>'
-        )
+        return Markup('<a href="' + self.report_url + '">result</a>')
+
+
+class AliyunReport(Model):
+    '''
+    table for storing Alibaba Cloud project
+    '''
+    __tablename__ = 'aliyun_report'
+    log_id = Column(Integer, primary_key=True)
+    ami_id = Column(String(50))
+    instance_type = Column(String(50))
+    instance_available_date = Column(Date, nullable=True)
+    compose_id = Column(String(50))
+    pkg_ver = Column(String(50))
+    bug_id = Column(String(50))
+    report_url = Column(String)
+    branch_name = Column(String(50))
+    cases_pass = Column(Integer)
+    cases_fail = Column(Integer)
+    cases_cancel = Column(Integer)
+    cases_other = Column(Integer)
+    cases_total = Column(Integer)
+    pass_rate = Column(Integer)
+    test_date = Column(Date, nullable=True)
+    comments = Column(String)
+    platform = Column(String(50))
+
+    def __repr__(self):
+        return self.log_id
+
+    def result_url(self):
+        return Markup('<a href="' + self.report_url + '">result</a>')
+
 
 
 class Ali_Report(Model):
@@ -92,24 +122,22 @@ class AzureReport(Model):
     failed_cases = Column(String)
     rerun_failed_cases = Column(String)
     duration = Column(Integer)
-    finished_time =  Column(Date, nullable=True)
+    finished_time = Column(Date, nullable=True)
     log = Column(String)
     platform = Column(String(20))
-    
+
     def __repr__(self):
         return self.index
 
     def log_link(self):
-        return Markup(
-            '<a href="' +
-            self.log +
-            '">log</a>'
-        )
+        return Markup('<a href="' + self.log + '">log</a>')
+
 
 
 class FailureType(Model):
     '''
-    general use: table for specify failure types, eg. product_bug, tool_bug, env_bug
+    general use: table for specify failure types, eg. product_bug, tool_bug,
+                 env_bug
     '''
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
@@ -121,7 +149,8 @@ class FailureType(Model):
 
 class FailureStatus(Model):
     '''
-    general use: table for specify failure status, like closed, open, on_qa, verified, blocker
+    general use: table for specify failure status, like closed, open, on_qa,
+                 verified, blocker
     '''
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
@@ -140,15 +169,17 @@ class Bugs(Model):
     case_name = Column(String(50))
     bug_id = Column(Integer, nullable=True)
     bug_title = Column(String(200), nullable=True)
-    failure_id = Column(Integer, ForeignKey(
-        "failure_status.id"), nullable=False)
+    failure_id = Column(Integer,
+                        ForeignKey("failure_status.id"),
+                        nullable=False)
     failure_status = relationship("FailureStatus")
     branch_name = Column(String(50), nullable=True)
     comments = Column(Text)
     last_update = Column(Date)
     create_date = Column(Date)
-    failure_type_id = Column(Integer, ForeignKey(
-        "failure_type.id"), nullable=False)
+    failure_type_id = Column(Integer,
+                             ForeignKey("failure_type.id"),
+                             nullable=False)
     failure_type = relationship("FailureType")
     identify_keywords = Column(Text)
     identify_debuglog = Column(Text)
@@ -161,11 +192,10 @@ class Bugs(Model):
         if self.bug_id:
             return Markup(
                 '<a href="https://bugzilla.redhat.com/show_bug.cgi?id=' +
-                str(self.bug_id) +
-                '">'+str(self.bug_id)+'</a>'
-            )
+                str(self.bug_id) + '">' + str(self.bug_id) + '</a>')
         else:
             return self.bug_id
+
 
 class TestCases(Model):
     '''
@@ -183,10 +213,10 @@ class TestCases(Model):
     azure_casename = Column(String(100))
     azure_owner = Column(String(50))
     azure_comments = Column(String)
-    ali_repo = Column(String)
-    ali_casename = Column(String(100))
-    ali_owner = Column(String(50))
-    ali_comments= Column(String)
+    aliyun_repo = Column(String)
+    aliyun_casename = Column(String(100))
+    aliyun_owner = Column(String(50))
+    aliyun_comments = Column(String)
     esx_repo = Column(String)
     esx_casename = Column(String(100))
     esx_owner = Column(String(50))
@@ -195,13 +225,14 @@ class TestCases(Model):
     hyperv_casename = Column(String(100))
     hyperv_owner = Column(String(50))
     hyperv_comments = Column(String)
-    create_date =  Column(Date, nullable=True)
-    last_update =  Column(Date, nullable=True)
-    create_by =  Column(String(50))
+    create_date = Column(Date, nullable=True)
+    last_update = Column(Date, nullable=True)
+    create_by = Column(String(50))
     comments = Column(Text)
 
     def __repr__(self):
         return self.case_id
+
 
 class Client(User):
     __tablename__ = "ab_user"
